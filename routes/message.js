bdc625759730dbc60b34f42bcb67380aaab9d9ba
Lucheezou <95588852+Lucheezou@users.noreply.github.com>
2023-04-10 20:43:54 +0800

    Update message.js

--- routes/message.js
+++ routes/message.js
@@ -3,19 +3,20 @@ const processMessage = require('./process');
 module.exports = (req, res) => {
 
   if (req.body.object === 'page') {
-    
+    let lastsent = ""
     req.body.entry.forEach(entry => {
       console.log(req.body.entry)
-      console.log(req.body.entry.messaging)
+     
       entry.messaging.forEach(event => {
+        console.log(entry.messaging)
         if (event.message && event.message.text) {
-        
-            return
-            
+  
+            if (lastsent === event.message.text){
+            return}
             console.log(event)
             processMessage(event)
-           
-        
+            
+            
         }
         
       });
