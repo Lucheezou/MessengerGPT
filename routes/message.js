f5ba39e05797796168994bc92c1eb6a06ab7b044
Lucheezou <95588852+Lucheezou@users.noreply.github.com>
2023-04-10 17:12:25 +0800

    fix multisend

--- routes/message.js
+++ routes/message.js
@@ -1,16 +1,19 @@
 const processMessage = require('./process');
 
 module.exports = (req, res) => {
+  let lastsent;
   if (req.body.object === 'page') {
     req.body.entry.forEach(entry => {
       entry.messaging.forEach(event => {
         if (event.message && event.message.text) {
+             
             if (lastsent === event.message.text){
               return;
             }
+            lastsent = event.message.text;
             console.log(event)
             processMessage(event)
-            let lastsent = event.message.text;
+           
         }
       });
     });
